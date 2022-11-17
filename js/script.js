fec069239a05d03c70f868f47a8c8d5d49321850
Pawel Nackowski <pawelnackowski@gmail.com>
2022-11-17 19:23:10 +0100

    adding fnction init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+{
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+    };
+
+    const init = () => {
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+
+    };
+
+    init();
+}
