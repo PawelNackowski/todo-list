ca40a56110a612a67a1e2fb26f784ea4849ebaf3
Pawel Nackowski <pawelnackowski@gmail.com>
2022-11-18 10:13:41 +0100

    added newTaskContent

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 {
+const task = [];
+
+const addNewTask = (newTaskContent) => {
+task.push({
+    content: newTaskContent,
+});
+console.log(newTaskContent);
+};
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
